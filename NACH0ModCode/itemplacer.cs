d45010f1ce3d9e07326a1f651b8b4ea6559269ac
NACH0CHEE5E <39394001+NACH0CHEE5E@users.noreply.github.com>
2019-05-10 22:36:29 +1000

    code stuffs

---
+++ NACH0ModCode/itemplacer.cs
@@ -0,0 +1,54 @@
+﻿using NetworkUI;
+using Pipliz;
+using Shared;
+using System.Collections.Generic;
+using UnityEngine;
+using Chatting;
+
+namespace NACH0.UI
+{
+    [ModLoader.ModManager]
+    public class ItemPlace
+    {
+        static string itemName = "NACH0.Types.ItemPlacer";
+        
+        [ModLoader.ModCallback(ModLoader.EModCallbackType.OnPlayerClicked, "NACH0.UI.ItemPlace.OnPlayerClick")]
+        public static void PlaceItem(Players.Player player, PlayerClickedData data)
+        {
+           
+            if (data.TypeSelected != ItemTypes.GetType(itemName).ItemIndex)
+            {
+                return;
+            }
+            if (data.TypeSelected == ItemTypes.GetType(itemName).ItemIndex)
+            {
+                if (data.ClickType == PlayerClickedData.EClickType.Left)
+                {
+                    SendGuardUI.SendUI(player);
+                }
+                else if (data.ClickType == PlayerClickedData.EClickType.Right)
+                {
+                    if (PlayerClickedData.EHitType.Block == data.HitType)
+                    {
+                        PlayerClickedData.VoxelHit voxelData = data.GetVoxelHit();
+                        if (voxelData.SideHit == VoxelSide.yPlus)
+                        {
+                            /*if (voxelData.DistanceToHit <= 10)
+                            {
+                                
+                            }*/
+                            ServerManager.TryChangeBlock(voxelData.PositionBuild, ItemTypes.GetType(GuardUIInteraction.item_placer_dict[player]).ItemIndex);
+                            //ServerManager.TryChangeBlock(voxelData.PositionBuild, ItemTypes.GetType("NACH0.Types.Slingshot.Guard.Night").ItemIndex);
+
+                            Chat.Send(player, "<color=blue>Placed: " + GuardUIInteraction.item_placer_dict[player] + "</color>");
+                        }
+                    }
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
